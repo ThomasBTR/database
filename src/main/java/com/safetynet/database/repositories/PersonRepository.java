167e9e92e1dbc23d756a60759bbf5388cc1be05a
Thomas BERTHOMIER <thomas.berthomier@capgemini.com>
2021-09-21 16:44:18 +0200

    modify pom to get only interfaces from spring swagger codegen

--- src/main/java/com/safetynet/database/repositories/PersonRepository.java
+++ src/main/java/com/safetynet/database/repositories/PersonRepository.java
@@ -1,10 +1,11 @@
 package com.safetynet.database.repositories;
 
 import com.safetynet.database.entities.PersonEntity;
+import com.safetynet.models.PersonJson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
-	public PersonEntity save(org.safetynet.models.Person person);
+	public PersonEntity save(PersonJson person);
 
 }
