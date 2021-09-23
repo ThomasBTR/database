b5547580db8446390e9de62b15f5bafa2f2ae43b
Thomas BERTHOMIER <thomas.berthomier@capgemini.com>
2021-09-23 08:57:07 +0200

    started to develop Firestation link from api to database

--- src/main/java/com/safetynet/database/repositories/PersonRepository.java
+++ src/main/java/com/safetynet/database/repositories/PersonRepository.java
@@ -1,11 +1,17 @@
 package com.safetynet.database.repositories;
 
+import com.safetynet.database.entities.MedicationEntity;
 import com.safetynet.database.entities.PersonEntity;
 import com.safetynet.models.PersonJson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
-	public PersonEntity save(PersonJson person);
+
+	@Query("SELECT p from PersonEntity p WHERE p.addressEntity.station =:variable")
+	List<PersonEntity> findPersonEntitiesByAddressEntityStation(@Param("stationNumber") int stationNumber);
 
 }
